50c409e4e350b41eceb30fbd995737ff7fd121bb
J Irving <j@lollyshouse.ca>
2018-07-02 23:13:23 -0400

    This is a private repo

--- project.clj
+++ project.clj
@@ -8,4 +8,6 @@
   ;; Use the chained credential provider - env credentials or a profile (set
   ;; AWS_PROFILE)
   ;; https://github.com/s3-wagon-private/s3-wagon-private#aws-credential-providers
-  :repositories {"private" {:url "s3p://maven-private-repo/releases/" :no-auth true}})
+  :repositories {"private" {:url "s3p://maven-private-repo/releases/"
+                            :no-auth true
+                            :sign-releases false}})
