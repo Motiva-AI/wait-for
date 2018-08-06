517d71cd1457f58404554280e0a4efd61ca0118a
Paul Lam <paul@quantisan.com>
2018-08-06 14:02:27 +0900

    circleci publish to clojars

--- project.clj
+++ project.clj
@@ -10,4 +10,8 @@
   ;; https://github.com/s3-wagon-private/s3-wagon-private#aws-credential-providers
   :repositories {"private" {:url "s3p://maven-private-repo/releases/"
                             :no-auth true
-                            :sign-releases false}})
+                            :sign-releases false}
+                 "releases" {:url           "https://clojars.org/repo"
+                             :username      "motiva-ai"
+                             :password      :env
+                             :sign-releases false}})
